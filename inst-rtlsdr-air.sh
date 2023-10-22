270d13cf0e6d8d64645ece952d8570cc8d01be08
martinkoschel <108628035+martinkoschel@users.noreply.github.com>
2023-10-22 16:12:00 -0700

    Update inst-rtlsdr-air.sh
    Revisions from manually installing. Revised to plain `cmake ../`

--- inst-rtlsdr-air.sh
+++ inst-rtlsdr-air.sh
@@ -20,32 +20,52 @@ apt-get -y install git
 echo "**********************************"
 echo "Getting Dependencies..."
 echo "**********************************"
+
+echo "RTLSDR-Air Dependencies"
 echo "Getting build_essential:"
 apt-get -y install build-essential
+
 echo "Getting cmake:"
 apt-get -y install cmake
+
 echo "Getting pkg-config:"
 apt-get -y install pkg-config
+
 echo "Getting libmp3lame-dev:"
 apt-get -y install libmp3lame-dev
+
 echo "Getting libshout3-dev:"
 apt-get -y install libshout3-dev
+
 echo "Getting libconfig++-dev:"
 apt-get -y install 'libconfig++-dev'
-echo "Getting libraspberrypi-dev:"
-apt-get -y install libraspberrypi-dev
+
+# The below is for Raspberry v1, v2, v3
+# echo "Getting libraspberrypi-dev:"
+# apt-get -y install libraspberrypi-dev
+
 echo "Getting libfftw3-dev:"
 apt-get -y install libfftw3-dev
+
 echo "Getting librtlsdr-dev:"
 apt-get -y install librtlsdr-dev
+
 echo "Getting libpulse-dev:"
 apt-get -y install libpulse-dev
+
+echo "SOAPY Dependencies"
+
+# SOAPY also has cmake as a dependency...
+
 echo "Getting g++:"
 apt-get -y install 'g++'
+
 echo "Getting libpython3-dev:"
 apt-get -y install libpython3-dev
+
 echo "Getting python3-numpy:"
 apt-get -y install python3-numpy 
+
 echo "Getting swig:"
 apt-get -y install swig
 
@@ -61,8 +81,8 @@ mkdir build
 cd build
 cmake ..
 make -j`nproc`
-sudo make install -j`nproc`
-sudo ldconfig #needed on debian systems
+make install -j`nproc`
+ldconfig #needed on debian systems
 SoapySDRUtil --info
 cd ~/
 
@@ -73,6 +93,7 @@ echo "**********************************"
 echo "**********************************"
 echo "Getting the RTLSDR-Airband Code..."
 echo "**********************************"
+
 cd
 wget -O RTLSDR-Airband-4.2.0.tar.gz https://github.com/szpajder/RTLSDR-Airband/archive/v4.2.0.tar.gz
 tar xvfz RTLSDR-Airband-4.2.0.tar.gz
@@ -84,10 +105,17 @@ echo "Compiling..."
 echo "**********************************"
 mkdir build
 cd build
+
+# The below seemed to have issues, producing a "Kernel Driver Detached" error when invoking rtlsdr-air -f
+# trying plain cmake ../
 # Writing all options in cmake parameters to cancel out prior settings from potential earlier build
-cmake -DPLATFORM=native -DNFM=ON -DRTLSDR=ON -DMIRISDR=OFF -DSOAPYSDR=ON -DPULSEAUDIO=ON -DPROFILING=OFF -DCMAKE_BUILD_TYPE=Release ../
+# cmake -DPLATFORM=native -DNFM=ON -DRTLSDR=ON -DMIRISDR=OFF -DSOAPYSDR=ON -DPULSEAUDIO=ON -DPROFILING=OFF -DCMAKE_BUILD_TYPE=Release ../
+
+cmake ../
 make
 make install
+
+cd ~/
 
 echo "**********************************"
 echo "Done with RTLSDR-Airband. Next: Icecast Server"
@@ -98,6 +126,7 @@ echo "Installing Icecast"
 echo "**********************************"
 
 apt install -y  icecast2
+
 echo "**********************************"
 echo "**********************************"
 echo "DONE!"
@@ -106,4 +135,6 @@ echo "**********************************"
 echo "Remember to edit the config files:"
 echo " /usr/local/etc/rtl_airband.conf "
 echo " /etc/icecast2/icecast.xml "
+echo "                                 "
+echo "Consider a reboot.               "
 echo "**********************************"
