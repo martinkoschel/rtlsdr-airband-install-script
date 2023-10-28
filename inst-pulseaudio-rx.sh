c2895f7708c3c0853ec9ced405dea70d9c5aceaa
martinkoschel <108628035+martinkoschel@users.noreply.github.com>
2023-10-27 17:16:47 -0700

    Create inst-pulseaudio-rx.sh
    initial commit

---
+++ inst-pulseaudio-rx.sh
@@ -0,0 +1,53 @@
+#! /bin/bash/
+
+# script to set up the Raspberry Pi to receive audio stream from RTLSDR-Airband via Pulse Audio
+# script by Martin Koschel 2023
+# rtlsdr-airband by charlie-foxtrot
+
+
+clear
+
+
+echo "***********************************"
+echo "***********************************"
+echo "****   Installing Pulse Audio   ***"
+echo "****         S E R V E R        ***"
+echo "****                            ***"
+echo "**** that means the audio will  ***"
+echo "**** play on the speakers       ***"
+echo "**** attached to this device.   ***"
+echo "***********************************"
+echo "***********************************"
+
+echo " "
+echo " "
+
+echo "**********************************"
+echo "First Things First - Update and Upgrade..."
+echo "**********************************"
+apt update
+apt full-upgrade -y
+
+echo "**********************************"
+echo " Getting Git......"
+echo "**********************************"
+apt-get -y install git
+
+echo "**********************************"
+echo "Getting Dependencies..."
+echo "**********************************"
+echo " Oops, looks like there are none..."
+
+echo "**********************************"
+echo " setting up PAPREFS tool.."
+echo "**********************************"
+apt-get -y install paprefs
+
+
+echo "**********************************"
+echo " DONE. "
+echo " "
+echo " Use GUI to configure PAPREFS! "
+echo " "
+echo " Suggest reboot now ..."
+echo "**********************************"
