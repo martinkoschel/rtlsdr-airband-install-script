2e902a4b8b5f05d408717209dff1632ee5f9d48c
martinkoschel <108628035+martinkoschel@users.noreply.github.com>
2023-10-27 17:41:39 -0700

    Update inst-rtlsdr-air.sh
    added sed to edit icecast config file to burst on connect = 0

--- inst-rtlsdr-air.sh
+++ inst-rtlsdr-air.sh
@@ -133,6 +133,11 @@ echo "**********************************"
 
 apt install -y  icecast2
 
+echo " Setting BURST ON CONNECT to OFF"
+
+sudo sed -i "s/<burst-on-connect>1<\/burst-on-connect>/<burst-on-connect>0<\/burst-on-connect>/" /etc/icecast2/icecast.xml
+
+
 echo "**********************************"
 echo "**********************************"
 echo "DONE!"
