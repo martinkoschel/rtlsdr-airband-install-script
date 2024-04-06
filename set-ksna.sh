3b52b0b17208b698f99f4c2f87e179120dec14bc
martinkoschel <martin.koschel@komatec.com>
2024-04-06 16:36:13 -0700

    Added switchout script

---
+++ set-ksna.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# script to switch config file to KSNA-Airband.conf
+
+cd ~/
+
+echo "Create RTLSDR-AIRBAND config file based on example file....."
+user_home=$(eval echo ~$SUDO_USER)
+source_file="$user_home/KSNA-Airband.conf"
+destination_file=/usr/local/etc/rtl_airband.conf
+cp "$source_file" "$destination_file"
+echo "KSNA Configuration File copied over. Adjust as necessary!"
+echo "Continuing...."
+echo " "
+echo "Restarting rtlsdr-air "
+echo " "
+
+systemctl restart rtl_airband
